675d8fd46cb70e4d7a519679fb72b755ad3af23a
Jay Jin <jayjin03@gmail.com>
2023-07-07 09:06:43 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,76 @@
+<?php
+// Include the AWS SDK for PHP
+require 'aws.phar';
+use Aws\S3\S3Client;
+require "predis/autoload.php";
+Predis\Autoloader::register();
+
+$instance_id = file_get_contents("http://instance-data/latest/meta-data/instance-id");
+
+// Database connection parameters
+$db_hostname = "localhost";
+$db_database = "web_demo";
+$db_username = "username";
+$db_password = "password";
+$db = open_db_connection($db_hostname, $db_database, $db_username, $db_password);
+
+// Image upload options
+$storage_option = "hd";	// hd or s3
+$hd_folder  = "uploads";
+$s3_region  = "us-east-2";
+$s3_bucket  = "your_s3_bucket_name";
+$s3_prefix  = "uploads";
+$s3_client  = null;
+$enable_cf  = false;
+$cf_baseurl = "http://xxxxxxxxxxxxxx.cloudfront.net/";
+if ($storage_option == "s3")
+{
+	$s3_client = S3Client::factory(array('region' => $s3_region, 'version' => 'latest'));
+}
+
+// Simulate latency, in seconds
+$latency = 0;
+
+// Cache configuration
+$enable_cache = false;
+$cache_type = "memcached";	// memcached or redis
+$cache_key  = "images_html";
+if ($enable_cache && ($cache_type == "memcached"))
+{
+	$cache = open_memcache_connection();
+}
+else if ($enable_cache && ($cache_type == "redis"))
+{
+	$cache = open_redis_connection();
+}
+
+function open_db_connection($hostname, $database, $username, $password)
+{
+	// Open a connection to the database
+	$db = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password);
+	return $db;
+}
+
+function open_memcache_connection()
+{	
+	// Open a connection to the memcache server
+	$mem = new Memcached();
+	$mem->addServer('web-demo.xxxxxx.0001.use2.cache.amazonaws.com', 11211);	// node 1
+	$mem->addServer('web-demo.xxxxxx.0002.use2.cache.amazonaws.com', 11211);	// node 2
+	$mem->addServer('web-demo.xxxxxx.0003.use2.cache.amazonaws.com', 11211);	// node 3
+	return $mem;
+}
+
+function open_redis_connection()
+{
+	$parameters = [
+    'tcp://web-demo.xxxxxx.clustercfg.use2.cache.amazonaws.com:6379'    // configuration endpoint
+	];
+	$options = [
+    	'cluster' => 'redis'
+	];
+
+	$redis = new Predis\Client($parameters, $options);
+	return $redis;
+}
+?>
---
+++ index.php
@@ -0,0 +1,288 @@
+<?php
+/*
+ *
+ * The first part handles application logic.
+ *
+ */
+
+include("config.php");
+session_start();
+$server   = $_SERVER['SERVER_ADDR'];
+
+// Simulate latency 
+sleep($latency);
+
+if (isset($_POST['username'])) 
+{
+	// This is a login request
+	process_login($_POST['username']);
+}
+
+if (isset($_GET['logout']))
+{
+	// This is a logout request
+	process_logout();
+}
+
+if (isset($_FILES["fileToUpload"]) && isset($_SESSION['username']))
+{
+	// Check file type before processing
+	$file_temp = "/tmp/".basename($_FILES["fileToUpload"]["name"]);
+	$file_size = $_FILES["fileToUpload"]["size"];
+	error_log("Handling $file_temp with $file_size bytes");
+
+	$file_type = strtolower(pathinfo($file_temp, PATHINFO_EXTENSION));
+	if(($file_type != "jpg") && ($file_type != "png") && ($file_type != "jpeg") && ($file_type != "gif") ) 
+	{
+		// Not an image file, ignore the upload request
+	}
+	else
+	{
+		$username = $_SESSION['username'];
+		// This is an image upload request, save the file first
+		if ($storage_option == "hd")
+		{
+			// In config.php, we specify the storage option as "hd"
+			$key = save_upload_to_hd($_FILES["fileToUpload"], $hd_folder);
+			add_upload_info($db, $username, $key);
+		}
+		else if ($storage_option == "s3")
+		{
+			// In config.php, we specify the storage option as "S3"
+			$key = save_upload_to_s3($s3_client, $_FILES["fileToUpload"], $s3_bucket, $s3_prefix);
+			add_upload_info($db, $username, $key);
+		}
+
+		if ($enable_cache)
+		{
+			// Delete the cached record, the user will query the database to get an updated version
+			if ($cache_type == "memcached")
+			{
+				$cache->delete($cache_key);
+			}
+			else if ($cache_type == "redis")
+			{
+				$cache->del($cache_key);
+			}
+		}
+	}
+}
+
+function process_login($username)
+{
+	// Simply write username to session data
+	$_SESSION['username'] = $username;
+}
+
+function process_logout()
+{
+	// Unset all of the session variables.
+	$_SESSION = array();
+
+	// If it's desired to kill the session, also delete the session cookie.
+	// Note: This will destroy the session, and not just the session data!
+	if (ini_get("session.use_cookies")) 
+	{
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+	}
+
+	// Finally, destroy the session.
+	session_destroy();
+}
+
+function save_upload_to_hd($uploadedFile, $folder)
+{
+	// Rename the target file with a UUID
+	$ext = pathinfo($uploadedFile["name"], PATHINFO_EXTENSION);
+	$uuid = uniqid();
+	$key = $uuid.".".$ext;
+
+	// Copy the upload file to the target file
+	$tgtFile  = $folder."/".$key;	
+	move_uploaded_file($uploadedFile["tmp_name"], $tgtFile);
+	return $key;
+}
+
+function save_upload_to_s3($s3_client, $uploadedFile, $s3_bucket, $s3_prefix)
+{
+	try 
+	{
+		// Rename the target file with a UUID
+		$ext = pathinfo($uploadedFile["name"], PATHINFO_EXTENSION);
+		$uuid = uniqid();
+		if (empty($s3_prefix))
+		{
+			$key = $uuid.".".$ext;		
+		}
+		else
+		{
+			$key = $s3_prefix."/".$uuid.".".$ext;		
+		}
+
+		// Upload the uploaded file to S3 bucket
+		$s3_client->putObject(array(
+			'Bucket' => $s3_bucket,
+			'Key'    => $key,
+			'SourceFile' => $uploadedFile["tmp_name"]
+  	        ));
+		error_log("Uploaded to S3 as s3://$s3_bucket/$key.");
+	} catch (S3Exception $e) 
+	{
+		echo "There was an error uploading the file to S3.\n";
+		return false;
+	}	
+
+	return $key;
+}
+
+function add_upload_info($db, $username, $filename)
+{
+	// Add a new record to the upload_images table
+	$sql = "INSERT INTO upload_images (username, filename) VALUES (?, ?)";	
+	$statement = $db->prepare($sql);
+	$statement->execute(array($username, $filename));
+}
+
+function retrieve_recent_uploads($db, $count)
+{
+	// Print a message so that the user knows these records come from the DB.
+	echo "Getting latest $count records from database.<br>";
+        error_log("Getting latest $count records from database.");
+
+	// Geting the latest records from the upload_images table
+	$sql = "SELECT * FROM upload_images ORDER BY timeline DESC LIMIT $count";
+	$statement = $db->prepare($sql);
+	$statement->execute();
+	$images = $statement->fetchAll(PDO::FETCH_ASSOC);
+	return $images;
+}
+
+function db_rows_2_html($images, $storage_option, $hd_folder, $s3_client, $s3_bucket, $enable_cf, $cf_baseurl)
+{
+	$html = "\n";
+	if ($enable_cf == true)
+	{
+	        // Images can be either on S3 or HD
+        	foreach ($images as $image)
+        	{
+                	$filename = $image["filename"];
+                	$url = $cf_baseurl.$filename;
+               	 	$html = $html."<img src='$url' width=200px height=150px>\n";
+        	}
+	}
+	else if ($storage_option == "hd")
+	{
+	        // Images are on hard disk
+        	foreach ($images as $image)
+        	{
+                	$filename = $image["filename"];
+                	$url = $hd_folder."/".$filename;
+               	 	$html = $html."<img src='$url' width=200px height=150px>\n";
+        	}
+	}
+	else if ($storage_option == "s3")
+	{
+        	// Images are on S3
+        	foreach ($images as $image)
+        	{
+                	$filename = $image["filename"];
+
+			//Create S3 presigned URL
+			$cmd = $s3_client->getCommand('GetObject',
+				['Bucket' => $s3_bucket, 'Key' => $filename]);
+			$req = $s3_client->createPresignedRequest($cmd, '+600 minutes');
+	                $url = (string) $req->getUri();
+                	$html = $html. "<img src='$url' width=200px height=150px>\n";
+        	}
+	}
+	return $html;
+}
+?>
+
+<?php
+/*
+ *
+ * The second part handles user interface.
+ *
+ */
+echo "<html>\n";
+echo "<head>\n";
+echo "<META http-equiv='Content-Type' content='text/html; charset=UTF-8'>\n";
+echo "<title>Scalable Web Application</title>\n";
+echo "<script src='demo.js'></script>\n";
+echo "</head>\n";
+echo "<body>\n";
+
+if (isset($_SESSION['username']))
+{
+	$username = $_SESSION['username'];
+	// This section is shown when user is login
+	echo "<table width=100% border=0>";
+	echo "<tr>";
+		echo "<td><H1>$server</H1></td>";
+		echo "<td align='right'>";
+			echo "$username<br>";
+			echo "<a href='index.php?logout=yes'>Logout</a>";
+		echo "</td>";
+	echo "</tr>";
+	echo "</table>";
+	echo "<HR>";
+
+	echo "In this demo, we assume that you are uploading images files with file extensions such as JPG, JPEG, GIF, PNG.<br>&nbsp;<br>";
+
+	echo "<form action='index.php' method='post' enctype='multipart/form-data'>";
+	echo "<input type='file' id='fileToUpload' name='fileToUpload' id='fileToUpload' onchange='check_file_type();'>";
+	echo "<input type='submit' value='Upload Image' id='submit_button' name='submit_button' disabled>";
+	echo "</form>";
+
+}
+else
+{
+	// This section is shown when user is not login
+	echo "<table width=100% border=0>\n";
+	echo "<tr>\n";
+		echo "<td><H1>$server</H1></td>\n";
+		echo "<td align='right'>\n";
+			echo "<form action='index.php' method='post'>";
+			echo "Enter Your Name: <br>";
+			echo "<input type='text' id='username' name ='username' size=20><br>";
+			echo "<input type='submit' value='login'/>";
+			echo "</form>\n";
+		echo "</td>\n";
+	echo "</tr>\n";
+	echo "</table>\n";
+	echo "<HR>\n";
+}
+
+// Get the most recent N images
+if ($enable_cache)
+{
+	error_log("Cache enabled, try to obtain the cached version.");
+	// Attemp to get the cached records for the front page
+	$images_html = $cache->get($cache_key);
+	if (!$images_html)
+	{
+		// If there is no such cached record, get it from the database
+		$images = retrieve_recent_uploads($db, 10, $storage_option);
+		// Convert the records into HTML
+		$images_html = db_rows_2_html($images, $storage_option, $hd_folder, $s3_client, $s3_bucket, $enable_cf, $cf_baseurl);
+		// Then put the HTML into cache
+		$cache->set($cache_key, $images_html);
+	}
+}
+else
+{
+	// This statement get the last 10 records from the database
+	$images = retrieve_recent_uploads($db, 10, $storage_option);
+	$images_html = db_rows_2_html($images, $storage_option, $hd_folder, $s3_client, $s3_bucket, $enable_cf, $cf_baseurl);
+}
+// Display the images
+echo $images_html;
+
+$session_id = session_id();
+echo "<hr>";
+echo "Session ID: ".$session_id;
+echo "\n</body>";
+echo "\n</html>";
+?>
